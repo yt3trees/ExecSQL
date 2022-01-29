bc08c0bc2441546b9ef61eea843f8da6a332cebc
yt3trees <y.atforest@gmail.com>
2022-01-29 15:32:00 +0900

    add build.cmd

--- build.cmd
+++ build.cmd
@@ -1,12 +1,12 @@
 rem upx --upx-dir C:\upx-3.96-win64
 rem --icon=icon/copytool.ico
-pyinstaller ExecSQL.py --name ExecSQL --onefile --noconsole
+pyinstaller ExecSQL.py --name ExecSQL --noconsole
 
 if exist .\app\ExecSQL\ExecSQL.exe (
     echo 上書きします。
-    echo A | xcopy .\dist .\app\ExecSQL /S
+    echo A | xcopy .\dist .\app /S
 ) else (
     echo 新規でコピーします。
-    echo D | xcopy .\dist .\app\ExecSQL /S
+    echo D | xcopy .\dist .\app /S
 )
 pause
